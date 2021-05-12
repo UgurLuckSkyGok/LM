1fcd8608bdd22b331f0082a4f87c28598efe8a96
UpperpikUgur <68004925+UpperpikUgur@users.noreply.github.com>
2021-05-12 17:00:41 +0300

    first commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+	private Level activeLevel;
+
+	private void Awake()
+	{
+		int levelIndex = PlayerPrefs.GetInt("Level", 1);
+	
+		activeLevel = Resources.Load("Levels/Level" + levelIndex) as Level;
+
+		if (activeLevel == null)
+		{
+			levelIndex = 1;
+			Debug.LogWarning("Level" + levelIndex + " is not found. First level loaded!");
+			PlayerPrefs.SetInt("Level", levelIndex);
+			activeLevel = Resources.Load("Levels/Level" + levelIndex) as Level;
+		}
+
+		CreateScene();
+	}
+
+	private void CreateScene()
+	{
+		foreach (Transform child in transform)
+		{
+			Destroy(child.gameObject);
+		}
+
+		GameObject level = Instantiate(activeLevel.levelPrefab, transform);
+	}
+}
---
+++ Assets/Scripts/MC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MC : MonoBehaviour
+{
+	[SerializeField] private Text levelText;
+
+	private void Start()
+	{
+		levelText.text = "Level :" + PlayerPrefs.GetInt("Level", 1);
+	}
+	public void NextLevel()
+	{
+		int levelndex = PlayerPrefs.GetInt("Level", 1);
+		levelndex++;
+		PlayerPrefs.SetInt("Level", levelndex);
+		SceneManager.LoadScene("Main");
+	}
+}
